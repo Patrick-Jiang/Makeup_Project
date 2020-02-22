cf6b021fecb8d1ef40e5a150a59dbb7efe6534fa
patrick.jiang <patrick.jiang86@gmail.com>
2020-02-22 00:42:01 -0600

    Created brands,products,tags controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'tags/index'
+  get 'tags/show'
+  get 'products/index'
+  get 'products/show'
+  get 'brands/index'
+  get 'brands/show'
   # get 'categories/index'
   # get 'categories/show'
   root to: 'categories#index'
