37ea12c35481251b62d60f0ebe61dcf4b77cb8cc
patrick.jiang <patrick.jiang86@gmail.com>
2020-02-22 00:13:46 -0600

    created categories controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'categories/index'
-  get 'categories/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'categories/index'
+  # get 'categories/show'
+  root to: 'categories#index'
+
+  get 'categories/:id', to: 'categories#show', id: /\d+/, as: 'category'
+  # For details on the DSL available within this file, see https:/ / guides.rubyonrails.org / routing.html
 end
