ee77ca3d13f9a7122e3a3320e8a82addbdd2a50f
patrick.jiang <patrick.jiang86@gmail.com>
2020-02-23 22:49:52 -0600

    Added pagy to Tags controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class TagsController < ApplicationController
+  include Pagy::Backend
   def index
-    @tags = Tag.all.page(params[:page])
+    @pagy, @tags = pagy(Tag.all)
   end
 
   def show
